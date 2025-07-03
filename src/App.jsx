9c2d1c39e547af0d7f769cc7eb4050ee5b142c5b
ihsanullahkhan11 <ihsan666880@gmail.com>
2025-07-03 17:42:56 +0500

    this is sample component made in reactjs

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+ 
+    <h1>ihsan ullah khan</h1>
+    </>
+  )
+}
+export default function Galler() {
+return(
+  <>
+  <App/>
+  <App/>
+  <App/>
+
+  </>
+)
+
+}
+   
+
