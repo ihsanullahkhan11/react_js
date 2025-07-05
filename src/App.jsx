816fb0c7f0dd3d9e36666f5d172a452ae02eac11
ihsanullahkhan11 <ihsan666880@gmail.com>
2025-07-05 20:45:56 +0500

    ma ne cards banaya props ki madad se our os ki andar css use keya our flex property use ki ya

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,25 @@ import StudentInfo from './components/Student';
 
 function App() {
   return (
+
     <div>
-      <h1>Student Information</h1>
+    <h1>Student Information</h1>
+   
+    <div 
+    style={{
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "space-between"
+    }}
+    >
+      
       <StudentInfo name="Ihsan Ullah" age="18" className="12th"  image="https://lnk.ink/xmods"/>
       <StudentInfo name="Abid Ullah khan" age="17" className="11th" image="https://lnk.ink/xmods" />
       <StudentInfo name="Aziz Ullah khan" age="16" className="10th" image="https://lnk.ink/xmods"/>
+      <StudentInfo name="asmat ullah khan" age="14" calssName="9th" image="https://lnk.ink/xmods" />
+     
     </div>
+     </div>
   );
 }
 
--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,38 +1,37 @@
 import React from "react";
-
 function StudentInfo(props){
-  return(
-    <div style={{
-      borderRadius: "10px",
-      color: "wheat",
-      background: "gray",
-      margin: "10px",
-      padding: "10px",
-      width: "100%",
-      height: "100%"
-    }}>
-     <h2>Student name: {props.name}</h2>
-     <p>Age: {props.age}</p>
-     <p>Class: {props.className}</p>
+ return(
+  <div style={{
+    borderRadius: "10px",
+    padding: "10px",
+    margin:   "10px",
+    background: "gray",
+    color: "wheat",
+  
+  }}>
+    <img 
+  src={props.image} 
+  alt={props.name} 
+  style={{
+    width:'100px',
+    height: '100px',
+    borderRadius: '50%'
+  }}
+   
+   />
 
-<div style={{
-  width : "100px",
-  height: "100px"
-}}>
+   
+      <h2>Student name: <br/> {props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Class {props.className}</p>
+  
+  
+
+  </div>
+ )
+}
 
 
    
-     <img 
-          src={props.image} 
-          alt={props.name} 
-          style={{
-            width: "100px",
-            height: "100px",
-            borderRadius: "50%" 
-       }} 
-     />
-     </div>
-    </div>
-  )
-}
+
 export default StudentInfo;
